2934da879bbd6ddf71227f8bc81e0d24c7dd56a1
murhum <dragomir.ivanov85@gmail.com>
2020-08-31 13:37:30 +0300

    SoMe FiXeS

--- endava_project.sh
+++ endava_project.sh
@@ -4,43 +4,27 @@ START=$(date +%s)
 
 echo "Good morning fellas. Let's get started."
 
-#First, let's create new dir with 'mkdir' and use 'cd' to enter
-mkdir endava_project
-cd endava_project
-
-#Then we use 'wget' command to download the content of 10 websites and '--output-document=' to name them
-wget --output-document=werock.html https://www.werock.bg
-wget --output-document=100beers.html https://100beers.bg
-wget --output-document=tangra.html http://www.radiotangra.com
-wget --output-document=dream.html https://www.dreamtravel.bg/
-wget --output-document=mail.html https://mail.bg/auth/login
-wget --output-document=endava.html https://www.endava.com/
-wget --output-document=abv.html https://www.abv.bg/
-wget --output-document=buena.html http://www.buenavistasocialclub.com/
-wget --output-document=wacken.html https://www.wacken.com/
-wget --output-document=tons.html https://70000tons.com/
-
-#For counting we will use 'grep -c href=' and >> sight to create a new file
-grep -c href= werock.html >> werock_href.txt
-grep -c href= 100beers.html >> 100beers_href.txt
-grep -c href= tangra.html >> tangra_href.txt
-grep -c href= dream.html >> dream_href.txt
-grep -c href= mail.html >> mail_href.txt
-grep -c href= endava.html >> endava_href.txt
-grep -c href= abv.html >> abv_href.txt
-grep -c href= buena.html >> buena_href.txt
-grep -c href= wacken.html >> wacken_href.txt
-grep -c href= tons.html >> tons_href.txt
+#First, let's create a variable with 10 random websites: 
+WEB=( https://www.werock.bg https://100beers.bg http://www.radiotangra.com https://www.dreamtravel.bg https://dir.bg https://www.endava.com http://www.buenavistasocialclub.com https://www.wacken.com https://askubuntu.com http://exotictour.bg/ )
 
 
-#Now we will delete all new 10 files using 'rm -rf *.txt' command
+#Then we use for loop to download the content 
+for element in "${WEB[@]}"; do
+        wget "$element"
+done
+
+#For counting we will use 'grep -c href=' 
+for i in index*; do
+        grep -c href= index* > result.txt;
+done
+
+unset i; while read -r l; do echo $l > "href$((++i)).txt";done < result.txt
+
+
+#Now we will delete all new files using 'rm -rf *.txt' command
 rm -rf *.txt
 
 #Maesure execution time
 END=$(date +%s)
 DIFF=$(( $END - $START ))
 echo "It took only $DIFF seconds. Cheers mate!"
-
-
-
-
